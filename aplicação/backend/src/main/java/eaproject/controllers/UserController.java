e566374ce0d9c0fc00a19a5cb56327a2fca15903
eduardoliv <cpm.6601@google.com>
2024-04-23 15:28:57 +0100

    Chore: First working service

--- aplicação/backend/src/main/java/eaproject/controllers/UserController.java
+++ aplicação/backend/src/main/java/eaproject/controllers/UserController.java
@@ -1,11 +1,16 @@
 package eaproject.controllers;
 
+import com.google.gson.Gson;
 import eaproject.beans.UserBean;
 import eaproject.dao.user;
+import eaproject.dto.userDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
 
 import javax.ejb.EJB;
+import java.util.Arrays;
+import java.util.List;
 
 @CrossOrigin("*")
 @RestController
@@ -15,12 +20,18 @@ public class UserController {
     UserBean userBean;
 
     @GetMapping("/")
-    public ResponseEntity<user[]> getAllUsers() {
+    public ResponseEntity<userDTO[]> getAllUsers() {
         user[] users = userBean.getAllUsers();
         if (users == null || users.length == 0) {
             return ResponseEntity.noContent().build();
         } else {
-            return ResponseEntity.ok(users);
+            userDTO[] usersList = new userDTO[users.length];
+            for(int i = 0; i < users.length; i++){
+                usersList[i] = new userDTO(users[i]);
+            }
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(usersList);
         }
     }
 }
---
+++ aplicação/backend/src/main/java/eaproject/dto/userDTO.java
@@ -0,0 +1,103 @@
+package eaproject.dto;
+
+import eaproject.dao.user;
+
+import java.io.Serializable;
+
+public class userDTO implements Serializable {
+    private int userId;
+    private String name;
+    private String email;
+    private String gender;
+    private int age;
+    private double height;
+    private double weight;
+    private boolean isActive;
+    private String registerDate;
+
+    // Constructor
+    public userDTO(user user) {
+        this.userId = user.getUserId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.gender = user.getGender();
+        this.age = user.getAge();
+        this.height = user.getHeight();
+        this.weight = user.getWeight();
+        this.isActive = user.getIsActive();
+        this.registerDate = user.getRegisterDate().toString();
+    }
+
+    // Getters and setters
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public String getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(String registerDate) {
+        this.registerDate = registerDate;
+    }
+}
