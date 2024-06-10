95cd3a79ede7f9a6cbbded40069fad7fc21d9b30
eduardoliv <cpm.6601@gmail.com>
2024-06-10 14:15:28 +0100

    Chore: update js models

--- aplicação/frontend/ea-project/src/models/input/UpdateCompetitionInput.js
+++ aplicação/frontend/ea-project/src/models/input/UpdateCompetitionInput.js
@@ -1,7 +1,7 @@
 import BaseInput from "./BaseInput";
 
 export default class UpdateCompetitionInput extends BaseInput {
-  constructor(id = 0, name = '', startDate = new Date(), endDate = new Date(), isActive = false, creationDate = new Date(), notificationIds = null, lazyLoad = false) {
+  constructor(id = 0, name = '', startDate = new Date(), endDate = new Date(), isActive = false, creationDate = new Date(), notificationIds = [], lazyLoad = false) {
     super(lazyLoad);
     this.id = id;
     this.name = name;
@@ -9,6 +9,6 @@ export default class UpdateCompetitionInput extends BaseInput {
     this.endDate = endDate;
     this.isActive = isActive;
     this.creationDate = creationDate;
-    this.notificationIds = notificationIds;
+    this.notificationIds = notificationIds.map(item => new UpdateCompetitionInput.Integer(item));
   }
 }
