4cba21b4fe4dbbfc31b3b9e3251fb5c08b8c6d67
MiguelSenra <miguelangelo.senra2002@gmail.com>
2024-06-18 10:15:54 +0100

    página de detalhes de competição

--- app/backend/src/main/java/eaproject/output/AuthenticationOutput.java
+++ app/backend/src/main/java/eaproject/output/AuthenticationOutput.java
@@ -6,30 +6,10 @@ public class AuthenticationOutput extends BaseOutput {
 
     @JsonProperty
     private String token;
-    @JsonProperty
-    private String role;
-    @JsonProperty
-    private int userId;
 
     public void setToken(String token) {
         this.token = token;
     }
 
     public String getToken() { return token; }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
 }
